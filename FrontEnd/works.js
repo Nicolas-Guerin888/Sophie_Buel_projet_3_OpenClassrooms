d9120a91727d43f60e61aa315c08ddd5441b04c4
Nicolas Guérin <nicolas.guerin03@gmail.com>
2024-05-31 10:54:12 +0200

    C6 - Optimisation des filtres avec un code dynamique

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,10 +1,7 @@
-let works = []
-
-// Envoi d'une requête GET à l'API via son URL
+// Envoi d'une requête GET /works à l'API via son URL
 fetch('http://localhost:5678/api/works')
 
-
-// Traitement de la promesse à la requête. Elle ensuite désérialiser si la promesse est tenue
+// Traitement de la promesse à la requête. Elle est ensuite désérialiser si la promesse est tenue
 .then(response => response.json())
 
 // Traitement de la promesse résolue avec les données JSON. Le paramètre works reçoit les données JSON de la promesse résolue. 
@@ -35,46 +32,6 @@ function displayGallery(works, selector){
         gallery.appendChild(figure)
     });
 }
-
-
-// Blocs de code pour les boutons filtres //
-
-// Bouton Tous //
-const btnTous = document.querySelector('.btn-tous')
-btnTous.addEventListener('click', function() {
-    displayGallery(works, ".gallery")
-console.log('Bouton tous cliqué')
-})
-
-// Bouton Objets // 
-const btnObjets = document.querySelector('.btn-objets')
-btnObjets.addEventListener('click', function() {
-        const objetsFiltres = works.filter(function(work) {
-            return work.category.name === "Objets";
-        });
-        console.log('bouton objets cliqué')
-        displayGallery(objetsFiltres, ".gallery")
-    })
-
-// Bouton Appartements // 
-const btnAppartements = document.querySelector('.btn-appartements')
-btnAppartements.addEventListener('click', function() {
-        const appartementsFiltres = works.filter(function(work) {
-            return work.category.name === "Appartements"
-        });
-        console.log('bouton appartements cliqué')
-        displayGallery(appartementsFiltres, ".gallery")
-    })
-    
-// Bouton Hotels & Restaurants //
-const btnHotelsRestaurants = document.querySelector('.btn-hotels\\&restaurants')
-btnHotelsRestaurants.addEventListener('click', function() {
-        const hotelRestoFiltre = works.filter(function(work) {
-            return work.category.name === "Hotels & restaurants"
-        });
-        console.log('bouton Hotels&restaurants cliqué')
-        displayGallery(hotelRestoFiltre, ".gallery")
-    })
 
 
 
